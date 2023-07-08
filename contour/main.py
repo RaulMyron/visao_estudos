145c2619b79f7cad74b2f749871453705ec89523
RaulMyron <raul.myron@gmail.com>
2023-07-08 11:49:13 -0300

    #

--- contour/main.py
+++ contour/main.py
@@ -1,13 +1,17 @@
 import numpy as np  
 import cv2
 
-im = cv2.imread('../imgs/camisa0.png')  
-#img = cv2.imread('imgs/defaultFrame.png')
+img = cv2.imread('../imgs/gwen.jpg')  
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
 
-cv2.imshow('img1',im)
-cv2.waitKey()
+cv2.imshow('Binary image', thresh)
+#cv2.imshow('ret', ret)
+cv2.waitKey(0)
+
 #cv2.imshow('imgs2',img)
 '''
+
 imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)  
 ret, thresh = cv.threshold(imgray, 127, 255, 0)  
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)  '''
